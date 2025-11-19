2fbac2744d7bd99b4d25404c3cc26864cf2a047b
6Morpheus6 <wanton-decay@gmx.net>
2025-11-19 02:44:36 +0100

    update
    - set active flag to use the active environment instead of creating a new one
    - install espeak
    - remove comments

--- install.js
+++ install.js
@@ -1,6 +1,5 @@
 module.exports = {
   run: [
-    // Edit this step to customize the git repository to use
     {
       method: "shell.run",
       params: {
@@ -9,31 +8,63 @@ module.exports = {
         ]
       }
     },
-    // Delete this step if your project does not use torch
     {
       method: "script.start",
       params: {
         uri: "torch.js",
         params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-          // triton: true   // uncomment this line if your project requires triton
-          // sageattention: true   // uncomment this line if your project requires sageattention
+          venv: "env",
+          path: "app",
+          // xformers: true,
+          // triton: true,
+          // sageattention: true
         }
       }
     },
-    // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
+        venv: "env",
+        path: "app",
         message: [
-          "uv pip install gradio devicetorch",
-          "uv pip install -r requirements.txt"
+          "uv pip install -e .",
+          "uv run --no-sync --active python docker/scripts/download_model.py --output api/src/models/v1_0"
         ]
       }
     },
+    {
+      when: "{{which('apt')}}",
+      method: "shell.run",
+      params: {
+        sudo: true,
+        message: "apt install libaio-dev espeak-ng"
+      },
+        next: "all"
+    },
+    {
+      when: "{{which('yum')}}",
+      method: "shell.run",
+      params: {
+        sudo: true,
+        message: "yum install libaio-devel espeak-ng"
+      },
+        next: "all"
+    },
+    {
+      when: "{{which('brew')}}",
+      method: "shell.run",
+      params: {
+        message: "brew install espeak-ng"
+      },
+        next: "all"
+    },
+    {
+      when: "{{which('winget')}}",
+      method: "shell.run",
+      params: {
+        sudo: true,
+        message: "winget install --id=eSpeak-NG.eSpeak-NG -e --silent --accept-source-agreements --accept-package-agreements"
+      }
+    }
   ]
 }
